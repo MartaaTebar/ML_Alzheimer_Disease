29cc9ddeb36a0e21efb84f2192e7908422aa2121
MartaaTebar <tebarmarta@gmail.com>
2025-03-30 13:13:14 +0200

    final step 1 and 2

---
+++ src/utils/confusion_matrix.py
@@ -0,0 +1,21 @@
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(y_test, y_pred, model_name):
+    '''
+    Function to easily draw the confusion matrix and see the results of the model evaluation.
+    - y_test: real results
+    - y_pred: predicted results
+    - model_name: name of the model for the plot
+    '''
+    
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6, 5))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=["Clase 0", "Clase 1"], yticklabels=["Clase 0", "Clase 1"])
+    plt.xlabel('Predicho')
+    plt.ylabel('Real')
+    plt.title(f'Matriz de Confusión - {model_name}')
+    plt.show()
